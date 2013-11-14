31433e7dea2143eb17e89042b1524dcfee6d73f4
Filip Jerzyna <filipjerzyna@gmail.com>
2013-11-14 12:46:02 +0100

    Final commit 2

--- AI_8Queens/src/pl/lodz/uni/math/GA8Queens/Population.java
+++ AI_8Queens/src/pl/lodz/uni/math/GA8Queens/Population.java
@@ -6,22 +6,21 @@ import java.util.List;
 import java.util.Random;
 
 /**
- *
+ * 
  * @author Admin
  */
 public class Population {
 
     private Random rand;
-    int boardSize=8;
+    int boardSize = 8;
     private int greatness = 0;
     private List<Board> chessBoards;
     private List<Board> newChessBoards;
-  
 
     public Population(int populationSize) {
         rand = new Random();
         chessBoards = new ArrayList<Board>();
-        newChessBoards = new ArrayList<Board>();//??? necessary?
+        newChessBoards = new ArrayList<Board>();
         for (int i = 0; i < populationSize; i++) {
             chessBoards.add(new Board());
         }
@@ -44,33 +43,30 @@ public class Population {
 
     }
 
-    //change to private!
-    //Bug here!
-    public boolean crossover(int firstBoard, int secondBoard) {
-        //zmienić!
-       // boolean probability = rand.nextInt(5) == 0;
-        boolean probability = true;
+    private boolean crossover(int firstBoard, int secondBoard) {
+
+        boolean probability = rand.nextInt(5) == 0;
         if (probability) {
 
             int length = 0;
             while (length == 0) {
                 length = rand.nextInt((boardSize) + 1);
             }
-            int position = -1;          
+            int position = -1;
             position = rand.nextInt(boardSize - (length - 1));
 
             int[] firstChild = chessBoards.get(firstBoard).getTiles().clone();
             int[] secondChild = chessBoards.get(secondBoard).getTiles().clone();
 
             for (int i = 0; i < length; i++) {
-                int temp=secondChild[position+i];
-                secondChild[position+i]=firstChild[position+i];
-                firstChild[position+i]=temp;
+                int temp = secondChild[position + i];
+                secondChild[position + i] = firstChild[position + i];
+                firstChild[position + i] = temp;
             }
             Board children1 = new Board();
             children1.setTiles(firstChild);
             newChessBoards.add(children1);
-            //2 dziecko
+
             Board children2 = new Board();
             children2.setTiles(secondChild);
             newChessBoards.add(children2);
@@ -87,7 +83,7 @@ public class Population {
         for (int i = 0; i < counter; i++) {
             int parentA = -1;
             int parentB = -1;
-            if (greatness>0) {
+            if (greatness > 0) {
                 int sum = 0;
                 int randomParent = rand.nextInt(greatness) + 1;
                 int iterator = 0;
@@ -97,7 +93,7 @@ public class Population {
                 }
                 parentA = iterator - 1;
                 parentB = iterator - 1;
-            
+
                 int criticalIterations = 100;
                 while (parentA == parentB) {
                     if (criticalIterations > 0) {
@@ -128,26 +124,14 @@ public class Population {
                 }
                 crossover(parentA, parentB);
             }
-         
+
         }
     }
 
-    
     private void newToTheOld() {
         chessBoards = newChessBoards;
-        //Collections.sort(chessBoards);  
-
-        // int quantity = newChromosomes.size() / 2;
         newChessBoards = new ArrayList<Board>();
-        //w tym miejscu chromosomy w newChromosomes musza być już posortowane 
-        //według funkcji oceny
-//        for (int i = 0; i < quantity; i++) {
-//            chromosomes.add(newChromosomes.get(i));
-//
-//        }
-//        newChromosomes = new ArrayList<Chromosome>();
     }
-
 
     protected int makeNewPopulation() {
         crossoverForAll();
@@ -156,7 +140,7 @@ public class Population {
         int bestId = -1;
         for (int i = 0; i < getChessBoards().size() && !best; i++) {
             mutate(i);
-           // transposition(i);
+            // transposition(i);
             int rating = getChessBoards().get(i).estimate();
             if (rating == 8) {
                 best = true;
@@ -174,17 +158,8 @@ public class Population {
             greatness += getChessBoards().get(i).getRate();
 
         }
-        this.greatness=greatness;
+        this.greatness = greatness;
     }
-
-/*    private int getPosition(int[] array, int number) {
-        for (int i = 0; i < array.length; i++) {
-            if (array[i] == number) {
-                return i;
-            }
-        }
-        return -1;
-    }*/
 
     /**
      * @return the greatness
